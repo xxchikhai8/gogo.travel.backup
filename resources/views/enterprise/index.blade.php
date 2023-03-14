16f1795286ff9d1c2bcc7feffbb7725deb4b8f66
Lê Chí Khải <khailcgcc19095@fpt.edu.vn>
2023-03-14 20:35:50 +0700

    Design Index for enterprise

--- resources/views/enterprise/index.blade.php
+++ resources/views/enterprise/index.blade.php
@@ -1,7 +1,41 @@
-@extends('enterprise.index');
+@extends('layouts.enterprise')
 @section('content')
 @section('title', 'Enterprise Home')
-@if (session('notify')=='enterprise')
+<div class="mb-3">
+    <table>
+        <tr>
+            <th>Flight ID</th>
+            <th>Plane ID</th>
+            <th>Departure</th>
+            <th>Destination</th>
+            <th>Departure Day</th>
+            <th>Flight Time</th>
+            <th>Return Day</th>
+            <th>Price Ticket</th>
+            <th>State</th>
+            <th>Config</th>
+        </tr>
+        @foreach ($flights as $flight)
+            <tr>
+                <td>{{$flight->flightID}}</td>
+                <td>{{$flight->planeID}}</td>
+                <td>{{$flight->departure}}</td>
+                <td>{{$flight->destination}}</td>
+                <?php $date = new DateTimeImmutable($flight->departDay); ?>
+                <td><?php echo date_format($date,'d M Y'); ?></td>
+                <td>{{$flight->flightTime}} Hours</td>
+                <?php $date = new DateTimeImmutable($flight->returnDay); ?>
+                <td><?php echo date_format($date,'d M Y'); ?></td>
+                <?php $money = $flight->priceTicket; setlocale(LC_MONETARY, 'en_US'); ?>
+                <td>$ <?php echo number_format($money); ?></td>
+                <td>{{$flight->state}}</td>
+                <td></td>
+            </tr>
+        @endforeach
+
+    </table>
+</div>
+@if (session('notify') == 'enterprise')
     <script>
         Swal.fire({
             title: 'Sign In Successfull!',
--- resources/views/layouts/enterprise.blade.php
+++ resources/views/layouts/enterprise.blade.php
@@ -4,13 +4,16 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.min.js"
         integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.3.0/js/fontawesome.min.js"
         integrity="sha256-Mag3WtYiA/1P91Qduc1FKNy/peKn5ptB8b7v7GFu5qQ=" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/enterprise.index.css">
+    <link rel="shortcut icon" href="/assets/img/favicon.ico" type="image/x-icon">
+    <title>@yield('title')</title>
 </head>
 
 <body class="d-flex flex-column h-100">
@@ -28,10 +31,16 @@
                             class="h2 mb-0">GGT Enterprise</span></a>
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link headertext" href="/">Home</a>
+                            <a class="nav-link headertext" href="/enterprise">Flight</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link headertext" href="/history">History</a>
+                            <a class="nav-link headertext" href="/planes">Plane</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link headertext" href="/airport">Airport</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link headertext" href="/ticket">Ticket</a>
                         </li>
                     </ul>
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
@@ -54,8 +63,23 @@
             </div>
         </nav>
     </header>
-    <main></main>
-    <footer></footer>
+
+    <main>
+        <div class="container-fluid mb-4">
+            @yield('content')
+        </div>
+    </main>
+
+    <footer>
+        <div class="row mb-2 text-center d-flex align-items-center">
+            <span><img src="/assets/img/GoGoLogo.svg" width="48px" height="48px" class="me-2"><span
+                    class="mb-0 h4">GoGo Travel</span></span>
+        </div>
+        <div class="row text-center">
+            <span>2023 &copy; GoGo Travel</span>
+        </div>
+    </footer>
+    <script src="/assets/js/enterprise.index.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
