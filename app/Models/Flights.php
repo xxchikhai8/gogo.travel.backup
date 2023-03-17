ac48cc5828ddd38b98bb99fd159bcdc584389693
Lê Chí Khải <khailcgcc19095@fpt.edu.vn>
2023-03-17 21:44:52 +0700

    Update Flight Model

--- app/Models/Flights.php
+++ app/Models/Flights.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Flight extends Model
+class Flights extends Model
 {
     use HasFactory;
     protected $fillable = [
